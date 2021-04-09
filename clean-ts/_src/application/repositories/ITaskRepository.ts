16df1adff7f9fe3eb3a75d685671da8dae112e4a
Komatsu Nobuyuki <abr0811@icloud.com>
2021-04-09 19:28:36 +0900

    ITaskRepository追加

---
+++ clean-ts/_src/application/repositories/ITaskRepository.ts
@@ -0,0 +1,8 @@
+import { Task } from '../../domain/models/Task';
+
+export abstract class ITaskRepository {
+  abstract findAll(): Promise<Array<Task>>
+  abstract findOne(): Promise<Task>
+  abstract update(): Promise<Task>
+  abstract delete(): Promise<Task>
+}
