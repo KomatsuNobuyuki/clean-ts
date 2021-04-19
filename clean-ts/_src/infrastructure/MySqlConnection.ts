bda2fe2dd3ebbf6cd2fc67d1b97bff733f99fff8
Komatsu Nobuyuki <abr0811@icloud.com>
2021-04-19 20:11:11 +0900

    getConnection時のコード追加

--- clean-ts/_src/infrastructure/MySqlConnection.ts
+++ clean-ts/_src/infrastructure/MySqlConnection.ts
@@ -1,4 +1,4 @@
-import mysql, { createPool } from 'mysql';
+import mysql from 'mysql';
 import dotenv from 'dotenv';
 import { IDBConnection } from '../interfaces/database/IDBConnection';
 
@@ -14,6 +14,20 @@ class MySqlConnection {
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
       timezone: 'utc'
+    });
+
+    this.pool.getConnection((error: mysql.MysqlError | undefined, connection: mysql.PoolConnection | undefined) => {
+      if(error) {
+        if(error.code === 'PROTOCOL_CONNECTION_LOST') {
+          console.error('Database connection was closed.');
+        } else if(error.code === 'ER_CON_COUNT_ERROR') {
+          console.error('Database has too many connections.');
+        } else if(error.code === 'ECONNREFUSED') {
+          console.error('Database connection was refused.');
+        }
+      }
+
+      if (connection) connection.release();
     });
   }
 }
