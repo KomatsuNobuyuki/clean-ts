f9ea8bb475f979e8305a20e4b341017c68d1702a
Komatsu Nobuyuki <abr0811@icloud.com>
2021-04-25 19:12:28 +0900

    TaskRepositoryにCRUDようのメソッド追加

--- clean-ts/_src/interfaces/database/TaskRepository.ts
+++ clean-ts/_src/interfaces/database/TaskRepository.ts
@@ -23,4 +23,37 @@ export class TaskRepository extends ITaskRepository {
     task.id = result.insertId
     return task
   }
+
+  async findOne(id: number): Promise<Task> {
+    const task = await this._connection.execute(
+      'select * from tasks where id = ? limit 1',
+      id
+    );
+
+    return task;
+  }
+
+  async findAll(): Promise<Array<Task>> {
+    const tasks = await this._connection.execute('select * from tasks');
+
+    return tasks;
+  }
+
+  async update(task: Task): Promise<Task> {
+    const updateTask = await this._connection.execute(
+      'update tasks set title = ?, description = ?, updated_at = ?, where id = ?',
+      [task.title, task.description, task.getJSTUpdetedAt(), task.id]
+    );
+
+    return updateTask;
+  }
+
+  async delete(task: Task): Promise<Task> {
+    const deleteTask = await this._connection.execute(
+      'delete tasks where id = ?',
+      task.id
+    );
+
+    return deleteTask;
+  }
 }
